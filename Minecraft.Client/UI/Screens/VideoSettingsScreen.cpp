fc7aea01b456399e4b465bc2b429987aa8f25470
jerryjhird <jerryjhird-public@proton.me>
2026-03-15 08:16:47 +0000

    added brightness and FOV sliders to grahpic settings as well as changed weird text in PauseScreen (also adds sliding logic to sliders so you can drag them with your mouse)

--- Minecraft.Client/UI/Screens/VideoSettingsScreen.cpp
+++ Minecraft.Client/UI/Screens/VideoSettingsScreen.cpp
@@ -18,28 +18,39 @@ void VideoSettingsScreen::init()
     Language *language = Language::getInstance();
     this->title = language->getElement(L"options.videoTitle");
 
-	// 4J - this was as static array but moving it into the function to remove any issues with static initialisation order
-	const Options::Option *items[8] = {
-			        Options::Option::GRAPHICS, Options::Option::RENDER_DISTANCE, Options::Option::AMBIENT_OCCLUSION, Options::Option::FRAMERATE_LIMIT, Options::Option::ANAGLYPH, Options::Option::VIEW_BOBBING,
-			        Options::Option::GUI_SCALE, Options::Option::ADVANCED_OPENGL
-	};
+    // We've expanded this to 10 items to include the "Hidden" ones
+    const Options::Option *items[10] = {
+        Options::Option::GRAPHICS,
+        Options::Option::RENDER_DISTANCE,
+        Options::Option::AMBIENT_OCCLUSION,
+        Options::Option::FRAMERATE_LIMIT,
+        Options::Option::ANAGLYPH,          
+        Options::Option::VIEW_BOBBING,
+        Options::Option::GUI_SCALE,
+        Options::Option::ADVANCED_OPENGL,
+        Options::Option::GAMMA,
+        Options::Option::FOV
+    };
 
-	for (int position = 0; position < 8; position++)
-	{
-		const Options::Option *item = items[position];
+    for (int position = 0; position < 10; position++)
+    {
+        const Options::Option *item = items[position];
+        int xPos = width / 2 - 155 + (position % 2 * 160);
+        int yPos = height / 6 + 24 * (position / 2);
+
         if (!item->isProgress())
-		{
-            buttons.push_back(new SmallButton(item->getId(), width / 2 - 155 + position % 2 * 160, height / 6 + 24 * (position >> 1), item, options->getMessage(item)));
+        {
+            buttons.push_back(new SmallButton(item->getId(), xPos, yPos, item, options->getMessage(item)));
         }
-		else
-		{
-            buttons.push_back(new SlideButton(item->getId(), width / 2 - 155 + position % 2 * 160, height / 6 + 24 * (position >> 1), item, options->getMessage(item), options->getProgressValue(item)));
+        else
+        {
+            buttons.push_back(new SlideButton(item->getId(), xPos, yPos, item, options->getMessage(item), options->getProgressValue(item)));
         }
     }
 
 //        buttons.add(new Button(VIDEO_BUTTON_ID, width / 2 - 100, height / 6 + 24 * 4 + 12, language.getElement("options.video")));
 //        buttons.add(new Button(CONTROLS_BUTTON_ID, width / 2 - 100, height / 6 + 24 * 5 + 12, language.getElement("options.controls")));
-    buttons.push_back(new Button(200, width / 2 - 100, height / 6 + 24 * 7, language->getElement(L"gui.done")));
+    buttons.push_back(new Button(200, width / 2 - 100, height / 6 + 24 * 6, language->getElement(L"gui.done")));
 
 }
 
@@ -47,25 +58,37 @@ void VideoSettingsScreen::buttonClicked(Button *button)
 {
     if (!button->active) return;
     if (button->id < 100 && (dynamic_cast<SmallButton *>(button) != NULL))
-	{
+    {
         options->toggle(((SmallButton *) button)->getOption(), 1);
         button->msg = options->getMessage(Options::Option::getItem(button->id));
+        ScreenSizeCalculator ssc(minecraft->options, minecraft->width, minecraft->height);
+        Screen::init(minecraft, ssc.getWidth(), ssc.getHeight());
+        return;
     }
     if (button->id == 200)
-	{
+    {
         minecraft->options->save();
         minecraft->setScreen(lastScreen);
+        return;
     }
-
-    ScreenSizeCalculator ssc(minecraft->options, minecraft->width, minecraft->height);
-    int screenWidth = ssc.getWidth();
-    int screenHeight = ssc.getHeight();
-    Screen::init(minecraft, screenWidth, screenHeight);	// 4J - was this.init
 }
 
 void VideoSettingsScreen::render(int xm, int ym, float a)
 {
     renderBackground();
     drawCenteredString(font, title, width / 2, 20, 0xffffff);
+
+    for (size_t i = 0; i < buttons.size(); ++i)
+    {
+        SlideButton* slider = dynamic_cast<SlideButton*>(buttons[i]);
+        
+        if (slider && slider->sliding)
+        {
+            const Options::Option* op = Options::Option::getItem(slider->id);
+            options->set(op, slider->value);
+            slider->msg = options->getMessage(op);
+        }
+    }
+
     Screen::render(xm, ym, a);
 }
